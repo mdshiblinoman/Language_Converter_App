c09aea99b596c5f5eec7634ec50cb18e135512ae
mdshiblinoman <mdshiblisharkar@gmail.com>
2026-03-13 10:48:16 +0600

    update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,28 +1,15 @@
-import { Tabs } from 'expo-router';
+import { Stack } from 'expo-router';
 import React from 'react';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
         name="index"
         options={{
           title: 'Converter',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="text.bubble.fill" color={color} />,
         }}
       />
-    </Tabs>
+    </Stack>
   );
 }
